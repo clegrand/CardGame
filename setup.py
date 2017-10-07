98b34c585d27a70dfc7c42fdda5f34d13528419b
clegrand <clegrand@student.42.fr>
2017-10-07 23:01:19 +0200

    Update URLs in setup

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     description='Basic cards game library',
     author='Christopher Legrand Soulimant',
     author_email='comachristo22@gmail.com',
-    url='https://github.com/clegrand/cards',
-    download_url='https://github.com/clegrand/cards/archive/0.1.tar.gz',
+    url='https://github.com/clegrand/Cards',
+    download_url='https://github.com/clegrand/Cards/archive/0.1.tar.gz',
     keywords=['cards', 'game', 'simple'],
     classifiers=[],
 )
