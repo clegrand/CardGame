9ee39b11b29cd56d85ae8c4bb9e2cb6a24ef565a
clegrand <comachristo22@gmail.com>
2017-11-19 16:18:24 +0100

    Try first PyPI setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='cards',
+    packages=['cards'],
+    version='0.1',
+    description='Basic cards game library',
+    author='Christopher Legrand Soulimant',
+    author_email='comachristo22@gmail.com',
+    url='https://github.com/clegrand/cards',
+    download_url='https://github.com/clegrand/cards/archive/0.1.tar.gz',
+    keywords=['cards', 'game', 'simple'],
+    classifiers=[],
+)
